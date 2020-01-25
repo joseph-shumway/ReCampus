e1deeaf0add8b4f454d4aef71c57a69fcc23b528
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-25 17:56:46 -0600

    -

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,4 +1,6 @@
 
+let a = $("#content div div div div div div div div div ul li a ").innerHTML
+console.log(`a is:`,a)
 
 document.head.innerHTML =  `<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
