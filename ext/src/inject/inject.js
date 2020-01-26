aa0fedaac3115a10cbea6b725ca2c200b9ac191d
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-25 21:57:59 -0600

    -

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,9 +1,23 @@
-
-let a = $("#content div div div div div div div div div ul li a ").innerHTML
-console.log(`a is:`,a)
-
-document.head.innerHTML =  `<!-- Compiled and minified CSS -->
+document.head.innerHTML = `<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>`
+
+
+let getMyCoursesElement = ()=>{
+    let sorted = [...document.querySelectorAll("div")].filter(each=>each.innerHTML.match(/<h2[\s\S]*<span[\s\S]*My Courses/g)).sort((a,b)=>a.innerHTML.length - b.innerHTML.length)
+    sorted[0].id = "myCourses"
+    return sorted[0]
+}
+
+setTimeout(()=>{
+    // sets the #myCourses id
+    getMyCoursesElement()
+
+    let classes = [...document.querySelectorAll("#myCourses li a")].map(each=>({href:each.href, name:each.name}))
+
+    localStorage.setItem("classes", JSON.stringify(classes))
+    
+    console.log(`JSON.parse(localStorage.getItem("classes")) is:`,JSON.parse(localStorage.getItem("classes")))
+},1000)
