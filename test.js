928457cd7696e7faa6dd1969abecbbb1a6fd9e5d
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-25 23:02:29 -0600

    - add materialize

--- test.js
+++ test.js
@@ -1,8 +1,22 @@
 let div = document.createElement("div")
-let classes = localStorage.getItem("classes")
+let classes = JSON.parse(localStorage.getItem("classes"))
 
-// classes.map(each=>`
-//     <input class=""
-// `)
-// for (let each of classes) {
-// }
+// create all the buttons
+div.innerHTML = classes.map(each=>`<a class="course-tab waves-effect waves-teal btn-large btn-flat">${each.name}</a>`).join("\n")
+
+div.style = `
+    display: flex;
+    min-height: 100vh;
+    width: 20rem;
+    background-color: gray;
+    align-items: flex-start;
+    justify-content: center;
+    font-size: 14pt;
+`
+div.classList.add("courseList")
+div.classList.add("grey")
+div.classList.add("darken-3")
+
+window.onload = _=>{
+    document.body.insertBefore(div, document.body.childNodes[0])
+}
