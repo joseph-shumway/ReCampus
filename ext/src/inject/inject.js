5ea7761ddf8494e54fc33bf4c95c395cef137226
Joseph Shumway <thegreenjag1@gmail.com>
2020-01-25 20:28:44 -0600

    -

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,14 +1,32 @@
-
-
-document.head.innerHTML =  `<!-- Compiled and minified CSS -->
+document.head.innerHTML = `<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>`
 
-document.body.innerHTML = `
-	<script>	
-		let a = $("#content div div div div div div div div div ul li a ").innerHTML
-		console.log(\`a is:\`,a)
-	</script>
-	`
+window.onload = async () => {
+  let contentLoaded = false
+
+  let hasContentLoadedYet = async () => {
+    return document.querySelectorAll("#content").length > 0
+  }
+  while (!contentLoaded) {
+    contentLoaded = await hasContentLoadedYet()
+  }
+
+  //   setTimeout(() => {
+  //     console.log(
+  //       document.querySelectorAll(
+  //         "#content div div div div div div div div div ul li a "
+  //       )
+  //     )
+  //   }, 1000)
+}
+
+window.addEventListener("", _ => {
+  console.log(
+    document.querySelectorAll(
+      "#content div div div div div div div div div ul li a "
+    )
+  )
+})
