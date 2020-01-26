0f64ec0fc70b89458fe005f396b5d8f8b373a9a9
Jeff Hykin <jeff.hykin@gmail.com>
2020-01-25 23:13:24 -0600

    - basic button and logo styling

--- test.js
+++ test.js
@@ -1,19 +1,26 @@
+// set the dummy classes
+localStorage.setItem("classes",`
+[
+    {"name":"CHEM 107", "href": ""},
+    {"name":"CHEM 107", "href": ""},
+    {"name":"CHEM 107", "href": ""},
+    {"name":"CHEM 107", "href": ""},
+    {"name":"CHEM 107", "href": ""}
+]`)
+
+
 let div = document.createElement("div")
 let classes = JSON.parse(localStorage.getItem("classes"))
 
 // create all the buttons
-div.innerHTML = classes.map(each=>`<a class="course-tab waves-effect waves-teal btn-large btn-flat">${each.name}</a>`).join("\n")
+div.id = "courseList"
+div.innerHTML = `
+    <div id=logoContainer>
+        ReCampus
+    </div>
 
-div.style = `
-    display: flex;
-    min-height: 100vh;
-    width: 20rem;
-    background-color: gray;
-    align-items: flex-start;
-    justify-content: center;
-    font-size: 14pt;
+    ${classes.map(each=>`<a class="courseTab waves-effect waves-teal btn-large btn-flat">${each.name}</a>`).join("\n")}
 `
-div.classList.add("courseList")
 div.classList.add("grey")
 div.classList.add("darken-3")
 
