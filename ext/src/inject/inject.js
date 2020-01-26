75e434265c61d80035c7993daec0f41b8e6e16cc
Joseph Shumway <thegreenjag1@gmail.com>
2020-01-25 19:50:07 -0600

    -

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,9 +1,14 @@
 
-let a = $("#content div div div div div div div div div ul li a ").innerHTML
-console.log(`a is:`,a)
 
 document.head.innerHTML =  `<!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>`
+
+document.body.innerHTML = `
+	<script>	
+		let a = $("#content div div div div div div div div div ul li a ").innerHTML
+		console.log(\`a is:\`,a)
+	</script>
+	`
