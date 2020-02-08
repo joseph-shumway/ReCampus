533026bebfaa70c807f5a9b9402d8a14808b0082
Jeff Hykin <jeff.hykin@gmail.com>
2020-02-08 15:03:47 -0600

    - added good-dom

--- test.js
+++ test.js
@@ -13,37 +13,32 @@ localStorage.setItem("classes",`
 // actual code
 // 
 //
-
+require("good-dom").global() // use a mini framework
 let classes = JSON.parse(localStorage.getItem("classes"))
 
 
 //
 // create root container
 //
-let rootContainer = document.createElement("div")
-rootContainer.id = "root"
+let classSidebar
+let rootContainer = new DIV(
+    {id:"root"},
+    // sidebar
+    classSidebar = new DIV(
+        {
+            id: "classSidebar",
+            innerHTML: `
+                <div id=logoContainer>
+                    ReCampus
+                </div>
 
-
-
-
-
-
-//
-// create classSidebar
-//
-
-let classSidebar = document.createElement("div")
-classSidebar.id = "classSidebar"
-classSidebar.innerHTML = `
-    <div id=logoContainer>
-        ReCampus
-    </div>
-
-    ${classes.map(each=>`<a class="courseTab waves-effect waves-teal btn-large btn-flat">${each.name}</a>`).join("\n")}
-`
+                ${classes.map(each=>`<a class="courseTab waves-effect waves-teal btn-large btn-flat">${each.name}</a>`).join("\n")}
+            `
+        },
+    )
+)
 classSidebar.classList.add("grey")
 classSidebar.classList.add("darken-3")
-rootContainer.appendChild(classSidebar)
 
 
 //
