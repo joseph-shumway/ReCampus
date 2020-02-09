fe85f215455030bf3888d4933f8043c436d2611f
Jeff Hykin <jeff.hykin@gmail.com>
2020-02-08 18:06:12 -0600

    - add got redirect working

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -4,36 +4,33 @@ document.head.innerHTML = `<!-- Compiled and minified CSS -->
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>`
 
+let eCampusLocation = null
+let url = window.location.href.replace(/https?:\/\//, "")
+if (url=="tamu.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_25_1") {
+    eCampusLocation = "home"
+    console.log("on homepage")
+} else {
+    eCampusLocation = "probablyAClass"
+    console.log("Probably on a course page")
+}
+
+
+
 let classes
-let main = ()=> {
-    console.log("Main just got called")
-    
+let loadedHome = ()=> {
     // 
-    // figure out which page we are on
+    // homepage
     // 
-    
-    // remove the http part
-    let url = window.location.href.replace(/https?:\/\//, "")
-    if (url=="tamu.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_25_1") {
-        // 
-        // homepage
-        // 
-        // save the classes to localStorage
-        localStorage.setItem("classes", JSON.stringify(classes))
-        
-        // FIXME: redirect to the first class's page
-        console.log(`classes is:`,classes)
+    // save the classes to localStorage
+    localStorage.setItem("classes", JSON.stringify(classes))
 
-        window.location.href = classes[0].href
-    } else if (url == "class page") {
-        // 
-    }
-
-
+    console.log(`classes is:`,classes)
+    // redirect to the first class's page
+    window.location.href = classes[0].href
 }
 
 let loader = async function() {
-
+    
     let getMyCoursesElement = () => {
         let sorted = [...document.querySelectorAll("div")].filter(each => each.innerHTML.match(/<h2[\s\S]*<span[\s\S]*My Courses/g)).sort((a, b) => a.innerHTML.length - b.innerHTML.length)
         sorted[0].id = "myCourses"
@@ -46,23 +43,30 @@ let loader = async function() {
         let classes = [...document.querySelectorAll("#myCourses li a")].map(each => ({ href: each.href, name: each.text }))
         return classes
     }
+
+    if (eCampusLocation == null || eCampusLocation == "home") {
+        // setup the interaval watcher
+
+        // try to see if the page loaded
+        let intervalWatcher
+        intervalWatcher = setInterval(()=>{
+            classes = []
+            try {
+                classes = getClassList()
+            } catch (error) {
+                
+            }
+            // if the classes were found
+            if (classes instanceof Array && classes.length > 0) {
+                // then stop looping and call the main function
+                clearInterval(intervalWatcher)
+                loadedHome()
+            }
+        }, 100)
+    } else if (eCampusLocation == "probablyAClass") {
+        console.log("not on home")
+    }
     
-    // try to see if the page loaded
-    let intervalWatcher
-    intervalWatcher = setInterval(()=>{
-        classes = []
-        try {
-            classes = getClassList()
-        } catch (error) {
-            
-        }
-        // if the classes were found
-        if (classes instanceof Array && classes.length > 0) {
-            // then stop looping and call the main function
-            clearInterval(intervalWatcher)
-            main()
-        }
-    }, 100)
 }
 // when the window loads, start main
 window.addEventListener("load", ()=>loader())
